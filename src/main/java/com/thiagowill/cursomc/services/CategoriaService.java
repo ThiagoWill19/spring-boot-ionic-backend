2facaf8298b5f93f00aa695a59741ff48a1b6151
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-09-09 17:32:04 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/thiagowill/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagowill/cursomc/services/CategoriaService.java
@@ -16,6 +16,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(int id) {
 		Optional<Categoria> obj =  repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new com.thiagowill.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
