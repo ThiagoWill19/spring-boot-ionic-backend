4338e7254eb5ef6838e39e4f0dc7320c4acf21a1
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-09-12 16:31:04 -0300

    Endpoint /pedidos/{id} disponivel

---
+++ src/main/java/com/thiagowill/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.thiagowill.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagowill.cursomc.domain.Pedido;
+import com.thiagowill.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(int id) {
+		Optional<Pedido> obj =  repo.findById(id);
+		return obj.orElseThrow(() -> new com.thiagowill.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
