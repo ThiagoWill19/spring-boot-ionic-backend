48a243d456044693e255dc087e18be80b3b18f69
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-09-12 14:00:26 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/thiagowill/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.thiagowill.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagowill.cursomc.domain.Cliente;
+import com.thiagowill.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(int id) {
+		Optional<Cliente> obj =  repo.findById(id);
+		return obj.orElseThrow(() -> new com.thiagowill.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
