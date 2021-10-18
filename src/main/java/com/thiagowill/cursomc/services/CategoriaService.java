801494bad44a3b354e42a15a105cad3006c956d1
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-10-18 15:55:55 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/thiagowill/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagowill/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagowill.cursomc.domain.Categoria;
+import com.thiagowill.cursomc.dto.CategoriaDTO;
 import com.thiagowill.cursomc.repositories.CategoriaRepository;
 import com.thiagowill.cursomc.services.exceptions.DataIntegrityException;
 
@@ -52,5 +53,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
