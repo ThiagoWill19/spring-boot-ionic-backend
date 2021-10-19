224dd978a5f82eee6dbfba8cfb28e21a25797f76
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-10-19 13:55:49 -0300

    Validação customizada: Email não repetido na atualização de Cliente

--- src/main/java/com/thiagowill/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/thiagowill/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.thiagowill.cursomc.domain.Cliente;
+import com.thiagowill.cursomc.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
